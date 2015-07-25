51babfc68c534d74527721eb3381311ea8a8c7e9
Anthony laO <anthonylao@live.com>
2015-07-25 05:44:35 +0000

    Added mailer and view

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,7 +9,7 @@ class ContactsController < ApplicationController
             if @contact.save
               name = params[:contact][:name]
               email = params[:contact][:email]
-              body = params[:body][:comments]
+              body = params[:contact][:comments]
               
               ContactMailer.contact_email(name, email, body).deliver
               flash[:success] = "Message Sent."
