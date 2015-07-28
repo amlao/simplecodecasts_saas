48cfda20a1f035b9ce66768e4b7afa50fabc448c
Anthony laO <anthonylao@live.com>
2015-07-28 00:09:39 +0000

    Reverted back to modified devise for  stripe

--- db/migrate/20150725080151_add_plan_to_user.rb
+++ db/migrate/20150725080151_add_plan_to_user.rb
@@ -1,5 +1,5 @@
-class AddStripeCustomerTokenToUsers < ActiveRecord::Migration
+class AddPlanToUser < ActiveRecord::Migration
   def change
-    add_column :users, :stripe_customer_token, :string
+    add_column :users, :plan_id, :integer
   end
 end
